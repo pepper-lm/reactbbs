f9965f9a41ff12f0a8942048a56d974e9e1721a5
luomin <841200357@qq.com>
2018-10-16 17:32:12 +0800

    搜索模态框基本完成

--- src/component/Head.jsx
+++ src/component/Head.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Button, Icon, Modal, Form, Input } from "antd";
 import banner from "../../public/images/head-banner.png"
 import '../css/head.css';
+import Search from "antd/lib/transfer/search";
 const FormItem = Form.Item;
 class Head extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             visible: false,//注册显示
-            loginVisible: false //登录模态框
+            loginVisible: false, //登录模态框
+            searchHidden: false //用于搜索框的显示
         }
 
     }
@@ -27,21 +29,26 @@ class Head extends React.Component {
         e.preventDefault();
         console.log("inter");
     }
+    handleSearch=()=>{
+        this.setState({
+            handleSearch:true
+        })
+    }
     render() {
         const { getFieldDecorator } = this.props.form;
 
         const signFootLayout = (
             <div className="footContent clearfix">
-                <Button type="primary" htmlType="submit" onClick={(e) => this.handleSignUpSubmit(e)} className="push-left">创建新账号</Button>
-                <Button className="push-left">登录</Button>
+                <Button type="primary" htmlType="submit" style={{ height: 16 }} onClick={(e) => this.handleSignUpSubmit(e)} className="push-left">创建新账号</Button>
+                <Button className="push-left" style={{ height: 16 }}>登录</Button>
                 <p className="push-left text-box" style={{ clear: "both" }}>注册即表示你同意<a href="#">隐私策略</a>和<a href="#">服务条款</a></p>
             </div>
         );
         const loginFootLayout = (
             <div className="footContent clearfix">
 
-                <Button  type="primary" className="push-left" htmlType="submit"><Icon type="unlock" theme="outlined" />登录</Button>
-                <Button  className="push-left">创建新账号</Button>
+                <Button type="primary" className="push-left" htmlType="submit"><Icon type="unlock" theme="outlined" />登录</Button>
+                <Button className="push-left">创建新账号</Button>
 
             </div>
         )
@@ -97,12 +104,26 @@ class Head extends React.Component {
                         </span>
                         <ul className="icon clearfix">
                             <li>
-                                <Icon type="search" theme="outlined" />
+                                <Icon type="search" theme="outlined" onClick={()=>this.handleSearch()}/>
                             </li>
                             <li>
                                 <Icon type="bars" theme="outlined" />
                             </li>
                         </ul>
+                        {this.state.searchHidden ?
+                            <div className="search-menu">
+                                <div className="menu-panel drop-down" style={{ top: "100%", width: 500, height: "auto" }}>
+                                    <div className="panel-body">
+                                        <div className="panel-body-contents clearfix">
+                                            <Input placeholder="搜索主题、帖子、用户或分类"></Input>
+                                            <div className="search-context clearfix">
+                                                <a href="#" className="widget-link show-help">选项</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> : ""
+                        }
                     </div>
                 </div>
                 {/** 注册的模态框*/}
