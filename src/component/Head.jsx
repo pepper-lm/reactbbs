7e8475222efff0f32d5dead76b2e4877630abde8
luomin <841200357@qq.com>
2018-10-16 14:11:13 +0800

    头部样式布局完成

--- src/component/Head.jsx
+++ src/component/Head.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import {Button,Icon} from "antd";
+import { Button, Icon } from "antd";
 import banner from "../../public/images/head-banner.png"
-class Head extends React.Component{
-    render(){
-        return(
-            <header>
-                <div>
-                    <img src={banner} alt="react"/>
+import '../css/head.css'
+class Head extends React.Component {
+    render() {
+        return (
+            <header className="react-head clearfix">
+                <div className="wrap clearfix">
+                    <div className="title">
+                        <a href="#">
+                            <img src={banner} alt="react" />
+                        </a>
+                    </div>
+                    <div className="panel clearfix">
+                        <span>
+                            <Button type="primary" className="sign-up-button">注册</Button>
+                            <Button type="primary" className="login-button"><Icon type="user" theme="outlined" />登录</Button>
+                        </span>
+                        <ul className="icon clearfix">
+                            <li>
+                                <Icon type="search" theme="outlined"  />
+                            </li>
+                            <li>
+                                <Icon type="bars" theme="outlined"  />
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <span>
-                        <Button type="primary">注册</Button>
-                        <Button><Icon type="user" theme="outlined" />登录</Button>
-                    </span>
-                     <ul>
-                        <li>
-                        <Icon type="search" theme="outlined" />
-                        </li>
-                        <li>
-                        <Icon type="bars" theme="outlined" />
-                        </li>
-                     </ul>
-                </div>
-            
+
+
             </header>
         )
     }
 }
 
-export  default Head
+export default Head
